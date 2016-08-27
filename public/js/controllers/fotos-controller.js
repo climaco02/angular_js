25204604b2bf5e552d2507c8c77659435f0621dc
climaco02 <climaco.cafe@gmail.com>
2016-08-27 01:09:21 -0300

    adding project

---
+++ public/js/controllers/fotos-controller.js
@@ -0,0 +1,17 @@
+ // fotos-controller.js
+
+angular.module('alurapic').controller('FotosController', function($scope, $http) {
+
+    $scope.fotos = [];
+    $scope.filtro = '';
+
+    $http.get('/v1/fotos')
+    .success(function(retorno) {
+    	console.log(retorno);
+    	$scope.fotos = retorno;
+    })
+    .error(function(erro) {
+    	console.log(erro);
+    });
+
+});
