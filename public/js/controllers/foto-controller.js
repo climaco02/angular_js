cf9aca7f7ce8cdc90c60fd5cdbe5972311a510bf
climaco02 <climaco.cafe@gmail.com>
2016-08-30 15:39:56 -0300

    changing controller to send picture to main page throw http

--- public/js/controllers/foto-controller.js
+++ public/js/controllers/foto-controller.js
@@ -1,8 +1,14 @@
 angular.module('alurapic')
-	.controller('FotoController', function($scope){
+	.controller('FotoController', function($scope, $http){
 		$scope.foto = {};
 
 		$scope.submeter = function(){
-			console.log($scope.foto);
+			$http.post('/v1/fotos', $scope.foto)
+			.success(function(){
+				console.log('Foto adicionada com sucesso');
+			})
+			.error(function(erro) {
+				console.log('Nao foi possivel cadastrar a foto');
+			})
 		};
 	});
